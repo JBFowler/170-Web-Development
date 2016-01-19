8a40adddd23f4487d6b624d414992d2c91d50f5a
Joe Fowler <jbfowler2@crimson.ua.edu>
2016-01-18 20:48:31 -0600

    Added search ability

--- lesson 4/book_viewer/book_viewer.rb
+++ lesson 4/book_viewer/book_viewer.rb
@@ -7,6 +7,10 @@ helpers do
       "<p>#{line}</p>"
     end.join
   end
+
+  def highlight(text, term)
+    text.gsub(term, %(<strong>#{term}</strong>))
+  end
 end
 
 before do
@@ -31,6 +35,25 @@ end
 
 get "/show/:name" do 
   params[:name]
+end
+
+get '/search' do
+
+  if params[:query]
+    # each result in this array will be:
+    # [ chapter name, chapter index, paragraph index ]
+    @results = @toc.each_with_index.each_with_object([]) do |(chapter, index), results|
+      text = File.read("data/chp#{index + 1}.txt")
+      paragraphs = text.split("\n\n")
+      paragraphs.each_with_index do |paragraph, paragraph_index|
+        if paragraph.include?(params[:query])
+          results << [chapter, index, paragraph, paragraph_index]
+        end
+      end
+    end
+  end
+
+  erb :search
 end
 
 not_found do
