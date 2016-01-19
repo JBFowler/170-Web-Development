eb900ecad6557d91ae308bbe37a1736129b681ee
Joe Fowler <jbfowler2@crimson.ua.edu>
2016-01-18 18:02:10 -0600

    Added layout

--- lesson 4/book_viewer/book_viewer.rb
+++ lesson 4/book_viewer/book_viewer.rb
@@ -2,8 +2,16 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  @title = "Some title"
+  @title = "The Adventures of Sherlock Holmes"
   @toc = File.readlines("data/toc.txt")
 
   erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @toc = File.readlines("data/toc.txt")
+  @ch1_text = File.read("data/chp1.txt")
+
+  erb :chapter
 end
