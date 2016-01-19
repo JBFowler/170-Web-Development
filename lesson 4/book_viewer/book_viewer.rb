4792216f6afe3e7ded268ed1965b81fbabe47f00
Joe Fowler <jbfowler2@crimson.ua.edu>
2016-01-18 18:19:11 -0600

    Added params to route

--- lesson 4/book_viewer/book_viewer.rb
+++ lesson 4/book_viewer/book_viewer.rb
@@ -8,10 +8,18 @@ get "/" do
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
+get "/chapters/:number" do
   @toc = File.readlines("data/toc.txt")
-  @ch1_text = File.read("data/chp1.txt")
+
+  number = params[:number].to_i
+  chapter_name = @toc[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+get "/show/:name" do 
+  params[:name]
 end
